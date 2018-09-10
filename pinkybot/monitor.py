f155d57b73f729dcda1a065393ba03a7cc408ca6
Your Name <you@example.com>
2018-09-10 17:41:46 +0700

    totalvolume

--- pinkybot/monitor.py
+++ pinkybot/monitor.py
@@ -27,7 +27,7 @@ class pinkybot(packselenium):
                     "order":"buy",
                     "stockname":configparams["stockname"].get(),
                     "startvalue":configparams["startvaluebuy"].get(),
-                    "startvolume":configparams["startvolumebuy"].get(),
+                    "startvolume":configparams["totalvolumebuy"].get(),
 
             }
             print ("Start:buy buy buy buy buy")
