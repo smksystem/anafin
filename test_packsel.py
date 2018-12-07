b3b82ba1ed7b5acfd9a64c73a592062fa2a8ece8
Your Name <you@example.com>
2018-12-07 09:54:43 +0700

    new

---
+++ test_packsel.py
@@ -0,0 +1,8 @@
+######Package of selenium class.
+
+class packselenium():
+	"""docstring for packselenium"""
+	def __init__(self):
+		pass
+	def login(self):
+		print("super selenium class is called")
---
+++ test_plugin/internal.py
@@ -0,0 +1,27 @@
+# internal.py
+
+class InternalPrinter:
+    """Internal business logic"""
+    def process(self):
+        print("Internal Hello")
+
+
+class MyApplication:
+    """First attempt at a plugin system"""
+    def __init__(self, *, plugins: list=list()):
+        self.internal_modules = [InternalPrinter()]
+        self.myplugins = plugins
+        print (self.myplugins)
+
+    def run(self):
+        print("Starting program")
+        print("-" * 79)
+
+        modules_to_execute = self.internal_modules + self.myplugins
+        print ( modules_to_execute)
+        for module in modules_to_execute:
+            module.process()
+
+        print("-" * 79)
+        print("Program done")
+        print()
