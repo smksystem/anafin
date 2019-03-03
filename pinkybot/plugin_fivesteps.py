c9eec5265685a48ec8f93fa890f74b22773a5427
myname <you@example.com>
2019-03-03 20:15:49 +0700

    new

--- pinkybot/plugin_fivesteps.py
+++ pinkybot/plugin_fivesteps.py
@@ -538,7 +538,8 @@ class fivesteps():
 								# buy price order
 								buyprice=  round((buyprice - (profitstep * commonvaluestep)),2)
 
-								print("\nprint buy value before processing line 462 plugin_fivesteps.py in def checkprocess2matchstatus")
+								print(
+									"\nprint buy value before processing line 462 plugin_fivesteps.py in def checkprocess2matchstatus")
 								print (buyprice)
 
 
